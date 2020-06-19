26b780b1680136e3bd27b756330a51433fbe860a
Lucas Zanella <me@lucaszanella.com>
2020-06-19 18:11:50 +0000

    fixed everything

--- src/main.cpp
+++ src/main.cpp
@@ -1,173 +1,31 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
-
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
-
-/*
-using namespace llvm;
-
-namespace {
-struct Hello : public FunctionPass {
-  static char ID;
-  Hello() : FunctionPass(ID) {}
-
-  bool runOnFunction(Function &F) override {
-    errs() << "Hello: ";
-    errs().write_escaped(F.getName()) << '\n';
-    return false;
-  }
-}; // end of struct Hello
-}  // end of anonymous namespace
-
-char Hello::ID = 0;
-static RegisterPass<Hello> X("hello", "Hello World Pass",
-                             false ,
-                             false );
-
-static RegisterStandardPasses Y(
-    PassManagerBuilder::EP_EarlyAsPossible,
-    [](const PassManagerBuilder &Builder,
-       legacy::PassManagerBase &PM) { PM.add(new Hello()); });
-*/
-
 #include "llvm/IR/Module.h"
-#include "llvm/IR/Function.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/IR/CallingConv.h"
 #include "llvm/IR/Verifier.h"
 #include "llvm/IR/IRPrintingPasses.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/DebugLoc.h"
-#include "llvm/IR/Instruction.h"
-#include "llvm/ADT/DenseMap.h"
-#include "llvm/ADT/FoldingSet.h"
-#include "llvm/ADT/SmallPtrSet.h"
-#include "llvm/ADT/StringMap.h"
-#include "llvm/IR/DebugInfo.h"
-#include "llvm/Support/Allocator.h"
-#include <iostream>
 
 using namespace llvm;
 
-namespace
-{
-    struct PrintPass : public ModulePass
-    {
-        static char ID;
-        PrintPass() : ModulePass(ID) {}
-
-        virtual bool runOnModule(Module &M)
-        {
-            //errs() << "I saw a function called " << F.getName() << "!\n";
-            for (Function &F : M.functions())
-            {
-                std::cout << "f" << std::endl;
-                for (BasicBlock &BB : F)
-                {
-                    for (Instruction &II : BB)
-                    {
-                        Instruction *I = &II;
-                        if (CallInst *CI = dyn_cast<CallInst>(I))
-                        {
-                            if (DILocation *Loc = I->getDebugLoc())
-                            {
-                                unsigned Line = Loc->getLine();
-                                StringRef File = Loc->getFilename();
-                                StringRef Dir = Loc->getDirectory();
-                                errs() << Dir << "/" << File << ":" << Line << "\n";
-                            }
-                        }
-                    }
-                }
-            }
-            errs() << "Module passed"
-                   << "!\n";
-            return false;
-        }
-    };
-} // namespace
-
-char PrintPass::ID = 0;
-
-// Register the pass so `opt -skeleton` runs it.
-static RegisterPass<PrintPass> X("printpass", "a pass that just prints the IR");
-
-namespace
-{
-    struct DummyPass : public FunctionPass
-    {
-    public:
-        static char ID;
-        DummyPass() : FunctionPass(ID) {}
-        virtual bool runOnFunction(Function &F) override;
-    };
-} // namespace
-
-bool DummyPass::runOnFunction(Function &F)
-{
-    errs() << "Visiting function " << F.getName();
-
-    for (BasicBlock &BB : F)
-    {
-        std::cout << "basicblock" << std::endl;
-        for (Instruction &II : BB)
-        {
-            std::cout << "instruction" << std::endl;
-            Instruction *I = &II;
-            std::cout << I->getOpcodeName() << std::endl;
-            if (CallInst *CI = dyn_cast<CallInst>(I))
-            {
-                std::cout << "CallInst *CI = dyn_cast<CallInst>(I)" << std::endl;
-                if (DILocation *Loc = I->getDebugLoc())
-                {
-                    unsigned Line = Loc->getLine();
-                    StringRef File = Loc->getFilename();
-                    StringRef Dir = Loc->getDirectory();
-                    errs() << Dir << "/" << File << ":" << Line << "\n";
-                }
-            }
-        }
-    }
-
-    return false;
-}
-
-// Register the pass with llvm, so that we can call it with dummypass
-char DummyPass::ID = 0;
-static RegisterPass<DummyPass> X2("dummypass", "Example LLVM pass printing each function it visits");
-
-using namespace llvm;
-
-Module *makeLLVMModule();
+Module *makeLLVMModule(LLVMContext& Context);
 
 int main(int argc, char **argv)
 {
-    Module *Mod = makeLLVMModule();
-    //PrintPass pp;
-    ModulePass *m = createPrintModulePass(outs(), "printing module:");
+    LLVMContext Context;
+    Module *Mod = makeLLVMModule(Context);
+    //Prints the module IR
+    ModulePass *m = createPrintModulePass(outs(), "Module IR printer");
     m->runOnModule(*Mod);
-    //pp.runOnModule(*Mod);
 
     return 0;
 }
 
-Module *makeLLVMModule()
+Module *makeLLVMModule(LLVMContext& Context)
 {
-    // Module Construction
-    LLVMContext Context;
-
-    Module *mod = new Module("test", Context);
+    Module *mod = new Module("mul_add", Context);
 
     FunctionType *FibFTy = FunctionType::get(Type::getInt32Ty(Context),
                                              {Type::getInt32Ty(Context),
@@ -191,10 +49,11 @@ Module *makeLLVMModule()
     Value *tmp2 = builder.CreateBinOp(Instruction::Add, tmp, z, "tmp2");
     builder.CreateRet(tmp2);
 
-    //DummyPass dp;
-    //dp.runOnFunction(*mul_add);
-    FunctionPass* fp = createPrintFunctionPass(outs(), "hello");
+    /*
+    //Prints function IR. Not needed as I fixed the module printer.
+    FunctionPass* fp = createPrintFunctionPass(outs(), "simple function IR printer");
     fp->runOnFunction(*mul_add);
+    */
 
     return mod;
 }
